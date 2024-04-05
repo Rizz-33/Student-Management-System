9f171264236df13b082e76cef3ee3843a89ef20d
Rizz-33 <162025795+Rizz-33@users.noreply.github.com>
2024-04-05 12:24:40 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/inclass_test/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.inclass_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/read.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ReadDetails extends StatelessWidget {
+  const ReadDetails({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Student Details"),
+      ),
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
